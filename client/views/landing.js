6754e3dc5ffd67b59de451490a20136095ba6da4
logansparlin <logan.sparlin@gmail.com>
2015-03-14 22:52:35 -0500

    previous / next functionality

--- client/views/landing.js
+++ client/views/landing.js
@@ -32,8 +32,8 @@ Template.landing.events({
 		var work = $('.work-container.active')
 		$('.pager-link').removeClass('link--active')
 		$(ev.target).addClass('link--active')
-		var currentIndex = Session.get('currentIndex')
-		var selectedIndex = $(ev.target).attr('data-index')
+		var currentIndex = Session.get('selectedIndex')
+		var selectedIndex = parseInt($(ev.target).attr('data-index'))
 		$('.work-container').removeClass('active')
 		TweenMax.fromTo(work, 0.5, {opacity:1,  y:'-50%'}, {opacity:0, y:'-60%', autoRound: false, force3D:true, ease: Expo.ease, display:'none', onComplete:showNew})
 		function showNew() {
@@ -45,6 +45,7 @@ Template.landing.events({
 				}
 			})
 		}
+		Session.set('selectedIndex', selectedIndex)
 	}
 });
 
@@ -60,6 +61,7 @@ Template.landing.rendered = function() {
 }
 
 Template.landing.rendered = function() {
+	$('.pager-link:first-of-type').addClass('link--active')
 	Session.set('selectedIndex', 1);
 	var index = Session.get('selectedIndex')
 	$('.work-container').each(function() {
@@ -69,7 +71,30 @@ Template.landing.rendered = function() {
 			_this.css('opacity', 1)
 
 		}
-	})
-	$('.pager-link:first-of-type').addClass('link--active')
+	});
+	$('.pager-link').each(function() {
+		var width = $(window).width()
+		var half = (width / 2)
+		var left = $(this).offset().left
+		if(left >= half) {
+			$(this).addClass('dark')
+		}
+	});
+	this.autorun(function() {
+		var data = Session.get('selectedIndex')
+		index = parseInt(Session.get('selectedIndex'))
+
+		// Previous and Next
+		var detectDirection = function(event, delta) {
+			if(delta < 0) {
+				console.log('previous')
+			}
+			if(delta > 0) {
+				console.log('next')
+			}
+		}
+		var changeData = _.debounce(detectDirection, 50, true);
+		$('body').on('mousewheel', changeData)
+	});
 
 }
